5efc391314796bc67db63595ad0b0f6ef01091a9
Ashutosh sharma <108721320+ashuison@users.noreply.github.com>
2025-06-18 17:16:25 +0530

    Update BaseTest.java

--- src/main/java/com/ashutosh/framework/base/BaseTest.java
+++ src/main/java/com/ashutosh/framework/base/BaseTest.java
@@ -1,5 +1,23 @@
 package com.ashutosh.framework.base;
 
-public class BaseTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
 
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null)
+            driver.quit();
+    }
 }
