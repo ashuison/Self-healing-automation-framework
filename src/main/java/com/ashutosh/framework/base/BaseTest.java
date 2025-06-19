7c796e263fa9e7f186515cfb0cb68e63a783c61c
Ashutosh sharma <108721320+ashuison@users.noreply.github.com>
2025-06-19 09:43:10 +0530

    Update BaseTest.java

--- src/main/java/com/ashutosh/framework/base/BaseTest.java
+++ src/main/java/com/ashutosh/framework/base/BaseTest.java
@@ -3,6 +3,7 @@ package com.ashutosh.framework.base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
 
 public class BaseTest {
@@ -11,13 +12,41 @@ public class BaseTest {
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-notifications");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(java.time.Duration.ofSeconds(30));
     }
 
     @AfterMethod
     public void tearDown() {
-        if (driver != null)
+        if (driver != null) {
             driver.quit();
+        }
+    }
+
+    /**
+     * Navigate to URL
+     */
+    protected void navigateTo(String url) {
+        driver.get(url);
+    }
+
+    /**
+     * Get current page title
+     */
+    protected String getPageTitle() {
+        return driver.getTitle();
+    }
+
+    /**
+     * Get current URL
+     */
+    protected String getCurrentUrl() {
+        return driver.getCurrentUrl();
     }
 }
